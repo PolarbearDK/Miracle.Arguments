e27785e6730eaeaa17f6076900941132aab37c9a
PolarbearDK <philip@dsdevelopment.dk>
2014-10-07 12:58:25 +0200

    Add additional targets (.Net 3.5 and .NET 4.5)

--- Source/Miracle.Arguments/BuildNuGetPackage.cmd
+++ Source/Miracle.Arguments/BuildNuGetPackage.cmd
@@ -1,4 +1,10 @@
 @echo off
-..\..\NuGet\NuGet.exe pack Miracle.Arguments.csproj -prop Configuration=release
-echo "run ..\..\NuGet\NuGet.exe push Miracle.Arguments.x.x.x.x.nupkg to publish"
+
+MSBUILD.exe Miracle.Arguments.csproj /t:BeforeBuild
+MSBUILD.exe Miracle.Arguments.csproj /t:Build /p:Configuration="Release 3.5"
+MSBUILD.exe Miracle.Arguments.csproj /t:Build /p:Configuration="Release 4.0"
+MSBUILD.exe Miracle.Arguments.csproj /t:Build /p:Configuration="Release 4.5"
+MSBUILD.exe Miracle.Arguments.csproj /t:AfterBuild;Package /p:Configuration="Release 4.5"
+
+echo "run ..\..\NuGet\NuGet.exe push NuGet\Miracle.Arguments... to publish"
 pause
